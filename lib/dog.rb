a3e2fb3080505eff429c7e704747da90c28efb7c
MMcClure11 <mcclme02@luther.edu>
2020-04-24 18:51:59 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,15 +46,11 @@ class Dog
 
   def self.find_by_id(id)
     sql = "SELECT * FROM dogs WHERE id = ?"
-    # result = DB[:conn].execute(sql, id)[0]
-    # Dog.new(id: result[0], name: result[1], breed: result[2])
     DB[:conn].execute(sql, id).map {|row| new_from_db(row)}.first
   end
 
   def self.find_by_name(name)
     sql = "SELECT * FROM dogs WHERE name = ?"
-    # result = DB[:conn].execute(sql, name)[0]
-    # Dog.new(id: result[0], name: result[1], breed: result[2])
     DB[:conn].execute(sql, name).map {|row| new_from_db(row)}.first
   end
 
