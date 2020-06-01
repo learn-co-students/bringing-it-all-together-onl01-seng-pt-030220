89125a6cf67e8176cf203e68b2bb6e378383f3d2
Dylan Williamson <dirtyassweed@icloud.com>
2020-06-01 22:16:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -33,24 +33,24 @@ class Dog
     self.new(id: id, name: name, breed: breed)
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
     sql = <<-SQL
-    SELECT *
-    FROM dogs
-    WHERE name = ?
-    LIMIT 1
+      SELECT *
+      FROM dogs
+      WHERE name = ?
+      LIMIT 1
     SQL
     DB[:conn].execute(sql, name).map do |row|
       self.new_from_db(row)
     end.first
   end
   
-  def self.find_by_id
+  def self.find_by_id(id)
     sql = <<-SQL
-    SELECT *
-    FROM dogs
-    WHERE id = ?
-    LIMIT 1
+      SELECT *
+      FROM dogs
+      WHERE id = ?
+      LIMIT 1
     SQL
     DB[:conn].execute(sql, id).map do |row|
       self.new_from_db(row)
