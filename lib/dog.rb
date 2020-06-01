158e047306cd7dae342a7f7fc5f1b16f7b4805ac
Dylan Williamson <dirtyassweed@icloud.com>
2020-06-01 22:14:36 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -16,14 +16,45 @@ class Dog
       id INTEGER PRIMARY KEY,
       name TEXT,
       breed TEXT
-      );
+      )
     SQL
     DB[:conn].execute(sql)
   end
   
   def self.drop_table
-    sql = "DROP TABLE IF EXISTS dog"
+    sql = "DROP TABLE IF EXISTS dogs"
     DB[:conn].execute(sql)
+  end
+  
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    self.new(id: id, name: name, breed: breed)
+  end
+  
+  def self.find_by_name
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE name = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+  
+  def self.find_by_id
+    sql = <<-SQL
+    SELECT *
+    FROM dogs
+    WHERE id = ?
+    LIMIT 1
+    SQL
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
 end
